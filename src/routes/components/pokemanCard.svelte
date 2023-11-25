41ec6652dcdf23ff5460eae4ee5a1d609d9bbb30
Long Vu <longvuwee@gmail.com>
2023-11-25 00:39:13 -0600

    Revert "Recipe Page"
    This reverts commit cf652ca1ec1fe1f9362717d414abab9917a6c62d.

--- src/routes/components/pokemanCard.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { fade } from 'svelte/transition';
-    export let pokeman;
-</script>
-<a class="m-4 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-lg flex flex-col items-center" href={`../pokemon/${pokeman.id}`}>
-    <img class="h-40 w-40" src={pokeman.image} alt={pokeman.image}>
-    <h2 class="capitalize text-2xl">{pokeman.id}. {pokeman.name}</h2>
-</a>
