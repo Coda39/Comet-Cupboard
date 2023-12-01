893576eacd83090308915173d0603add18a62110
Mateo Estrada <Mateo@Arelis-Air-2.lan1>
2023-12-01 17:26:45 -0600

    adding in progress events page

---
+++ src/routes/Events/+page.svelte
@@ -0,0 +1,18 @@
+<!--
+  v0 by Vercel.
+  https://v0.dev/t/FymEbI8vHCL
+-->
+<div class="min-h-screen flex items-center justify-center bg-base-100">
+  <div class="text-center space-y-6">
+    <img src="/Error.png"  alt="Maintenance" class="mx-auto" style="aspect-ratio: 200 / 200; object-fit: cover;">
+    <h1 class="text-3xl font-bold">Coming soon</h1>
+    <p class="text-lg text-accent-content">
+			We are currently working on this page. Check back soon to see our progress!
+    </p>
+    <a class="inline-block mt-4" href="/">
+      <button class="btn bg-primary">
+        Go Home
+      </button>
+    </a>
+  </div>
+</div>
