3b9eadbef61b0d67708b3366fb8763920c1c2e84
Mateo Estrada <Mateo@Arelis-Air-2.lan>
2023-12-01 14:53:51 -0600

    mostly fininshed main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 		  <h1 class="mb-5 text-5xl font-bold">Comet Cupboard</h1>
 		  <p class="mb-5">Erupting Flavors with Comet Magic</p>
 		<a  href = "#main-section" on:click={() => animateScroll.scrollTo({element: '#main-section', duration:750})}>
-		  <button class="btn btn-primary">Learn More</button>
+		  <button class="btn glass btn-primary">Learn More</button>
 		</a>
 		</div>
 	  </div>
@@ -27,7 +27,7 @@
 </div>
 
 <main id="main-section">
-<section class="w-full md:py-24 lg:py-32 grid items-center justify-center px-8 ">
+<section class="w-full md:py-24 lg:py-24 grid items-center justify-center px-8 ">
   <div class="container grid grid-cols-1 lg:grid-cols-3 px-4 md:px-6">
     <div class="lg:col-span-2 space-y-6 ">
       <h1 class="text-4xl font-bold tracking-tighter">Mission</h1>
@@ -59,7 +59,7 @@
     </div>
     <div class="grid grid-cols-2 gap-16 items center">
 	<div>
-		<div class="card w-52 bg-base-200 h-1/2 shadow-xl md:h-48">
+		<div class="card w-52 bg-base-200 h-1/2 shadow-xl md:h-48 hover:shadow-primary duration-300">
 		  <div class="card-body">
 			<h2 class="card-title text-center">Over 12,000 students served</h2>
 			<div class="card-actions justify-center">
@@ -69,7 +69,7 @@
 		</div>
 	</div>
 	<div>
-		<div class="card w-52 bg-base-200 shadow-xl h-1/2 md:h-48">
+		<div class="card w-52 bg-base-200 shadow-xl h-1/2 md:h-48 hover:shadow-primary duration-300">
 		  <div class="card-body">
 			<h2 class="card-title text-center">Over 300 volunteers/year</h2>
 			<div class="card-actions justify-center">
@@ -82,67 +82,77 @@
 
 </section>
 
-<div class="flex w-full">
-  <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
-  <div class="divider divider-horizontal">OR</div>
-  <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
-</div>
+ <section>
+   <div class="items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
+     <div class="text-left">
+       <div class="items-center mx-auto lg:inline-flex">
+         <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+           <div>
+             <p class="text-2xl font-semibold tracking-tight sm:text-4xl">
+				A brand new way to interact with the comet cupboard.
+             </p>
+           </div>
+           <div class="lg:ml-auto">
+             <p class="mt-4 text-lg tracking-tight lg:mt-0">
+				We are striving to improve the resources and the experience of 
+				using the comet cupboard one feature at a time.
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div>
+       <div class="pt-12 mx-auto lg:max-w-7xl">
+         <dl class="grid grid-cols-1 gap-6 space-y-0 lg:gap-24 lg:grid-cols-3">
+           <div>
+             <div>
+               <p class="text-lg font-bold leading-6">
+					New Interative Recipes page
+               </p>
+             </div>
+             <div class="mt-2 text-base ">
+					A better way to pick what to eat. Choose healthier and higher value foods that you can combine with what you have at home to cook up food that lasts.
+             </div>
+           </div>
+           <div>
+             <div>
+               <p class="text-lg font-bold leading-6">
+					Events page
+               </p>
+             </div>
+             <div class="mt-2 text-base ">
+					Keep up with all the events that are happening relating to the Comet Cupboard.
+
+             </div>
+           </div>
+           <div>
+             <div>
+               <p class="text-lg font-bold leading-6">
+					Growing and creating your own your own food.
+               </p>
+             </div>
+             <div class="mt-2 text-base ">
+					Have all the resources that the Comet Cupboard provides when it comes to learning about growing and eating natural and healthy foods in one place.
+             </div>
+           </div>
+         </dl>
+       </div>
+     </div>
+   </div>
+ </section>
 
 <section>
-	<ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-	  <li>
-		<div class="timeline-middle">
-		  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+	<div class="hero py-12 ">
+	  <div class="hero-content flex-col lg:flex-row">
+		<img alt ="comet cupboard staff" src="https://dygz37jdyaml.cloudfront.net/images/oue/cupboard/cupboard-02.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+		<div>
+		  <h1 class="text-5xl font-bold">Food shouldn't be a concern as a student</h1>
+		  <p class="text-lg py-6">As a community we strive to give everyone the things that they need and should receive. Don't let hunger stop you from succeding.</p>
+		<a href = "/Recipes">
+		  <button class="btn btn-primary">Get Started now</button>
+		</a>
 		</div>
-		<div class="timeline-start md:text-end mb-10">
-		  <div class="text-lg font-black">First Macintosh computer</div>
-		  The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh personal computer. It played a pivotal role in establishing desktop publishing as a general office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed in a beige case with integrated carrying handle; it came with a keyboard and single-button mouse.
-		</div>
-		<hr/>
-	  </li>
-	  <li>
-		<hr />
-		<div class="timeline-middle">
-		  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
-		</div>
-		<div class="timeline-end mb-10">
-		  <div class="text-lg font-black">iMac</div>
-		  iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has evolved through seven distinct forms
-		</div>
-		<hr />
-	  </li>
-	  <li>
-		<hr />
-		<div class="timeline-middle">
-		  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
-		</div>
-		<div class="timeline-start md:text-end mb-10">
-		  <div class="text-lg font-black">iPod</div>
-		  The iPod is a discontinued series of portable media players and multi-purpose mobile devices designed and marketed by Apple Inc. The first version was released on October 23, 2001, about 8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450 million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At over 20 years, the iPod brand is the oldest to be discontinued by Apple
-		</div>
-		<hr />
-	  </li>
-	  <li>
-		<hr />
-		<div class="timeline-middle">
-		  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
-		</div>
-		<div class="timeline-end mb-10">
-		  <div class="text-lg font-black">iPhone</div>
-		  iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone accounts for 15.6% of global smartphone market share
-		</div>
-		<hr />
-	  </li>
-	  <li>
-		<hr />
-		<div class="timeline-middle">
-		  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
-		</div>
-		<div class="timeline-start md:text-end mb-10">
-		  <div class="text-lg font-black">Apple Watch</div>
-		  The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness tracking, health-oriented capabilities, and wireless telecommunication, and integrates with iOS and other Apple products and services
-		</div>
-	  </li>
-	</ul>
+	  </div>
+	</div>
 </section>
 </main>
