bb18452c5648a357f7ca52766aaf8c4f8ece26ad
Mateo Estrada <Mateo@Arelis-Air-2.lan1>
2023-12-01 17:04:26 -0600

    added page transcitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,23 @@
 <script>
 import "../app.pcss";
+import { fade } from 'svelte/transition'
 import Header from "./components/header.svelte";
 import Footer from "./components/footer.svelte"
+
+export let data 
+$: pathname = data.pathname
 </script> 
 
-<div class = "h-full">
+{#key pathname}
+<div 
+class = "h-full"
+in:fade={{duration: 300, delay: 400}}
+out:fade={{duration:300}}
+>
 	<Header />
 	<main>
 		<slot/>
 	</main>
 	<Footer />
 </div>
+{/key}
